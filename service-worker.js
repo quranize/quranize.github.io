db170c74cfae7427ab1176bd54241215ca12f6ba
Alfan Nur Fauzan <alpancs@gmail.com>
2022-07-01 22:30:21 +0700

    handle chrome-extension scheme

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 self.addEventListener("fetch", event => {
     event.respondWith(
         fetch(event.request)
-            .then(response => caches
+            .then(response => event.request.url.startsWith("http") ? caches
                 .open("quranize-network-first")
                 .then(cache => {
                     cache.put(event.request, response.clone());
                     return response;
-                })
+                }) : response
             )
             .catch(() => caches.match(event.request))
     );
