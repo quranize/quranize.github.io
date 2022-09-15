0b59c17e64524ce7cd9419352b15b39bfb46e48d
Alfan Nur Fauzan <alpancs@gmail.com>
2022-09-15 23:53:17 +0700

    Service Worker: implement stale-while-revalidate for fonts (#12)
    * prepare for respondStaleWhileRevalidate for fonts
    
    * more resilient on cache errors
    
    * implement stale-while-revalidate
    
    * refactor
    
    * produce silent error

--- web-app/service-worker.js
+++ web-app/service-worker.js
@@ -1,13 +1,40 @@
 self.addEventListener("fetch", event => {
+    if (event.request.method == "GET" && /^https?:\/\//.test(event.request.url))
+        intercept(event);
+});
+
+function intercept(event) {
+    if (/\.(otf|ttf|woff|woff2)(\?.*)?$/.test(event.request.url))
+        respondStaleWhileRevalidate(event)
+    else
+        respondNetworkFirst(event);
+}
+
+function respondStaleWhileRevalidate(event) {
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            let fetchedResponse = fetch(event.request).then(response => {
+                putResponse(event.request, response);
+                return response;
+            }).catch(() => { });
+            return cachedResponse ? cachedResponse : fetchedResponse;
+        })
+    );
+}
+
+function respondNetworkFirst(event) {
     event.respondWith(
         fetch(event.request)
-            .then(response => event.request.url.startsWith("http") ? caches
-                .open("quranize-network-first")
-                .then(cache => {
-                    cache.put(event.request, response.clone());
-                    return response;
-                }) : response
-            )
+            .then(response => {
+                putResponse(event.request, response);
+                return response;
+            })
             .catch(() => caches.match(event.request))
     );
-});
+}
+
+async function putResponse(request, response) {
+    let clonedResponse = response.clone();
+    const cache = await caches.open("quranize-sw-v2");
+    return await cache.put(request, clonedResponse);
+}
