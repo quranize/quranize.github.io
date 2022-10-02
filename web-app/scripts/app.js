a1a515f9e96cb6e590ec164c78bc200e50e541bf
Alfan Nur Fauzan <alpancs@gmail.com>
2022-10-02 22:13:52 +0700

    increase the min harfs param to 29

--- web-app/scripts/app.js
+++ web-app/scripts/app.js
@@ -33,7 +33,7 @@ Vue.createApp({
     },
     methods: {
         initQuranize() {
-            this.quranize = new Quranize(25);
+            this.quranize = new Quranize(29);
             let URLHash = location.hash.replace(/^#/, "");
             if (this.keyword) this.encodeResults = this.quranize.encode(this.keyword)
             else if (URLHash) {
