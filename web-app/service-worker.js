e73add5434e05282e163d00660604dfa1d531ddb
Alfan Nur Fauzan <alpancs@gmail.com>
2022-10-16 22:08:40 +0700

    revert service worker behavior
    to use stale-while-revalidate + network-first

--- web-app/service-worker.js
+++ web-app/service-worker.js
@@ -1,8 +1,15 @@
 self.addEventListener("fetch", event => {
-    if (event.request.method == "GET" && /^https?:\/\//.test(event.request.url)
-        && !/(service-worker\.js|index\.html)$/.test(event.request.url))
-        respondStaleWhileRevalidate(event);
+    if (event.request.method == "GET" && /^https?:\/\//.test(event.request.url))
+        intercept(event);
 });
+
+function intercept(event) {
+    if (
+        /\.(otf|ttf|woff|woff2)(\?.*)?$/.test(event.request.url) ||
+        /\/scripts\/quran\/.*$/.test(event.request.url)
+    ) respondStaleWhileRevalidate(event)
+    else respondNetworkFirst(event);
+}
 
 function respondStaleWhileRevalidate(event) {
     event.respondWith(
@@ -16,8 +23,19 @@ function respondStaleWhileRevalidate(event) {
     );
 }
 
+function respondNetworkFirst(event) {
+    event.respondWith(
+        fetch(event.request)
+            .then(response => {
+                putResponse(event.request, response);
+                return response;
+            })
+            .catch(() => caches.match(event.request))
+    );
+}
+
 async function putResponse(request, response) {
     let clonedResponse = response.clone();
-    const cache = await caches.open("quranize-sw-v3");
+    const cache = await caches.open("quranize-sw-v2");
     return await cache.put(request, clonedResponse);
 }
