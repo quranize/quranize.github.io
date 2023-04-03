eef0d99838d23443cd0b7a159284a6085ad9a26a
Alfan Nur Fauzan <alpancs@gmail.com>
2023-04-03 23:48:05 +0700

    increase init value of quranizeCap

--- web-app/scripts/app.js
+++ web-app/scripts/app.js
@@ -2,7 +2,7 @@ import init, { Quranize } from "./quranize.js";
 import suraNames from "./quran/sura-names.js";
 
 await init();
-let quranizeCap = 13;
+let quranizeCap = 23;
 let quranize = new Quranize(quranizeCap);
 
 Vue.createApp({
