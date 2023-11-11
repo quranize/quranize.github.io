f8c491ff74100eeb3f41dea46f38aed66d47efdb
Alfan Nur Fauzan <alpancs@gmail.com>
2023-11-11 22:58:22 +0700

    increase the delay of initQuranize to 3125ms

--- web-app/scripts/app.js
+++ web-app/scripts/app.js
@@ -122,7 +122,7 @@ createApp({
             history.pushState({}, "", location.href.replace(/#.*$/, ""));
         }
         this.$refs.keyword.focus();
-        setTimeout(this.initQuranize, 3000, "");
+        setTimeout(this.initQuranize, 3125, "");
     },
 }).mount("#quranize-app");
 
