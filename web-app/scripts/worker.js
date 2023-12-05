faed4d94d902ea252397646eb452356ce66e5421
Alfan Nur Fauzan <alpancs@gmail.com>
2023-12-05 23:38:23 +0700

    make the worker more robust

--- web-app/scripts/worker.js
+++ web-app/scripts/worker.js
@@ -5,5 +5,5 @@ const quranize = new Quranize();
 
 self.onmessage = event => {
     const { method, args } = event.data;
-    self.postMessage({ method, args, returnValue: quranize[method](...args) });
+    self.postMessage({ method, args, returnValue: quranize[method]?.(...args) });
 };
